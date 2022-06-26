96ea387c678f80258473888154ace8a8bb242376
Graig <graig.mantle@gmail.com>
2022-06-26 13:20:03 -0700

    completed project on local host

--- backend/app/api.py
+++ backend/app/api.py
@@ -18,6 +18,56 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+todos = [
+    {
+        "id": "1",
+        "item": "Read a book."
+    },
+    {
+        "id": "2",
+        "item": "Cycle around town."
+    },
+    {
+        "id": "3",
+        "item": "Finish this project."
+    }
+]
+
+@app.get("/todo", tags=["todos"])
+async def get_todos() -> dict:
+    return { "data": todos }
+
+@app.post("/todo", tags=["todos"])
+async def add_todo(todo: dict) -> dict:
+    todos.append(todo)
+    return {
+        "data": { "Todo added." }
+    }
+
+@app.put("/todo/{id}", tags=["todos"])
+async def update_todo(id: int, body: dict) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todo["item"] = body["item"]
+            return {
+                "data": f"Todo with id {id} has been updated."
+            }
+    
+    return {
+        "data": f"Todo with id {id} not found."
+    }
+
+@app.delete("/todo/{id}", tags=["todos"])
+async def delete_todo(id: int) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todos.remove(todo)
+            return {
+                "data": f"Todo with id {id} has been removed."
+            }
+    return {
+        "data": f"Todo with id {id} not found."
+    }
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
